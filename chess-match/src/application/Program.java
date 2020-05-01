4718353c222627cc77e7f37388363239d398b567
gsfelix99 <gsfelixgit@gmail.com>
2020-05-01 10:42:50 -0300

    Moving Piece

--- chess-match/src/application/Program.java
+++ chess-match/src/application/Program.java
@@ -1,15 +1,30 @@
 package application;
 
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
 import boardgame.Board;
 import boardgame.Position;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPices());
+        while (true) {
+            UI.printBoard(chessMatch.getPices());
 
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
