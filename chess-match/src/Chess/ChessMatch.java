d8c9291eb3a61a5835a31262b6ab3e779c845edf
gsfelix99 <gsfelixgit@gmail.com>
2020-05-02 00:34:14 -0300

    Possible Moves

--- chess-match/src/Chess/ChessMatch.java
+++ chess-match/src/Chess/ChessMatch.java
@@ -49,6 +49,9 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
         }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("There is not a possible moves");
+        }
     }
 
 
