b2aabd21f118c7f4b853ea8ede74f0540daac3a5
Gabriel Felix <gsfelixgit@gmail.com>
2020-05-04 02:10:27 -0300

    Check Logic

--- chess-match/src/application/UI.java
+++ chess-match/src/application/UI.java
@@ -56,10 +56,16 @@ public class UI {
         System.out.println();
         printCapturedPieces(capturedPiece);
 //        System.out.println();
-        System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()){
-            System.out.println("Check !!");
+//        System.out.println("Turn: " + chessMatch.getTurn());
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Turn: " + chessMatch.getTurn());
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("Check !!");
+            }
+        } else {
+            System.out.println("CHECKMATE !!!");
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
         }
     }
 
